032c00fce2f4b33e9b3dd2018b2613c6bd173b61
Jose Carlos de Jesus Junior <juninho_web@hotmail.com>
2022-05-03 08:46:22 -0300

    Updating nuget packages dependencies

---
+++ TestProject1/Base/BaseTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit.Abstractions;
+
+namespace Framework.Core.Tests
+{
+    public abstract class BaseTest
+    {
+        #region| Fields |
+
+        protected ITestOutputHelper output;
+
+        #endregion
+
+        #region| Constructor |
+
+        public BaseTest(ITestOutputHelper testOutputHelper)
+        {
+            this.output = testOutputHelper;
+        }
+        #endregion
+    }
+}
